fe581e90ca3ef68a4726ef901c4fa6cdd236f76d
KübraKÖSE <kubraergul22@gmail.com>
2023-04-25 17:22:36 +0300

    completed header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { enableRipple } from '@syncfusion/ej2-base';
 import { MenuItemModel } from '@syncfusion/ej2-angular-navigations';
 enableRipple(true);
@@ -9,43 +9,86 @@ enableRipple(true);
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public errorNum = '5';
   public warningNum = '0';
   public successNum = '0';
+  public isChecked:boolean = false;
+  public menuItems: MenuItemModel[] | undefined
   constructor() {
    
   }
 
-  public isChecked:boolean = true;
-    // Menu items definition 
-  public menuItems: MenuItemModel[] = [
-    {
-      text: this.successNum,
-      iconCss: 'e-icons e-bullet-green',
-    },
-    {
-      text: this.warningNum,
-      iconCss: 'e-icons e-bullet-yellow',
-    },
-    {
-      text: this.errorNum,
-      iconCss: 'e-icons e-bullet-red',
-    },
-    {
-      iconCss:'e-icons e-checked',
-    },
-    {
-      text: 'Language',
-      items: [
-        { text: 'English' },
-        { text: 'Turkish' }
-      ]
-    },
-    {
-      iconCss: 'custom-icon logo'
+  ngOnInit() {
+   if(this.isChecked == true ) {
+    this.menuItems = [
+      {
+        text: this.errorNum,
+        iconCss: 'e-icons e-bullet-red',
+      },
+      {
+        text: this.warningNum,
+        iconCss: 'e-icons e-bullet-orange',
+      },
+      {
+        text: this.successNum,
+        iconCss: 'e-icons e-bullet-blue',
+      },
+  
+      {
+        iconCss:'e-icons e-checked',
+      },
+      {
+        text: 'Language',
+        items: [
+          { text: 'English' },
+          { text: 'Turkish' }
+        ]
+      },
+      {
+        iconCss: 'custom-icon logo'
+  
+      }
+    ];
 
-    }
-  ];
+   }
+
+   else {
+    this.menuItems = [
+      {
+        text: this.errorNum,
+        iconCss: 'e-icons e-bullet-red',
+      },
+      {
+        text: this.warningNum,
+        iconCss: 'e-icons e-bullet-orange',
+      },
+      {
+        text: this.successNum,
+        iconCss: 'e-icons e-bullet-blue',
+      },
+  
+      {
+        iconCss:'e-icons e-remove',
+      },
+      {
+        text: 'Language',
+        items: [
+          { text: 'English' },
+          { text: 'Turkish' }
+        ]
+      },
+      {
+        text: 'MCS',
+        iconCss: 'custom-icon logo'
+  
+      }
+    ];
+   }
+  }
+
+
+   
+ 
 }
